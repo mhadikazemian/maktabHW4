942ff1617fc1473480891c9d358287e5e906e795
kazemian <mkzn94@gmail.com>
2019-03-16 00:11:16 +0330

    Fixes main

--- q1/Q1.java
+++ q1/Q1.java
@@ -4,12 +4,6 @@ import java.util.Scanner;
 
 public class Q1 {
     public static void main(String[] args) {
-        int number;
-        String sentence;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your sentence: ");
-        sentence = scanner.nextLine();
-        System.out.println("Enter your desired number: ");
-        number = scanner.nextInt();
+
     }
 }
