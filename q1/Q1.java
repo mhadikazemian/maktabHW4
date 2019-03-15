7684adca6794111e46d57c1a5e45c911e6309235
kazemian <mkzn94@gmail.com>
2019-03-15 20:36:02 +0330

    Adds main method to test classes and adds the messages that are going to print for user

--- q1/Q1.java
+++ q1/Q1.java
@@ -1,4 +1,15 @@
 package com.maktab.HW4.q1;
 
+import java.util.Scanner;
+
 public class Q1 {
+    public static void main(String[] args) {
+        int number;
+        String sentence;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your sentence: ");
+        sentence = scanner.nextLine();
+        System.out.println("Enter your desired number: ");
+        number = scanner.nextInt();
+    }
 }
