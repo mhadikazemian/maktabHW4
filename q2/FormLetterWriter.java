aeaf9ec43c3937f74a984afef9c485a0e85bfa36
kazemian <mkzn94@gmail.com>
2019-03-16 15:36:17 +0330

    Completes FormLetterWriter class

--- q2/FormLetterWriter.java
+++ q2/FormLetterWriter.java
@@ -1,4 +1,14 @@
 package com.maktab.HW4.q2;
 
 public class FormLetterWriter {
+
+    public void displaySalutation(String lastName){
+        System.out.println("Dear Mr. or Ms" + lastName);
+        System.out.println("Thank you for your recent order");
+    }
+    public void displaySalutation(String firstName, String lastName){
+        System.out.println("Dear " + firstName + " " + lastName);
+        System.out.println("Thank you for your recent order");
+
+    }
 }
