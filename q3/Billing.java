8591af00adcaf7d27518f236a4e87f0386140b57
kazemian <mkzn94@gmail.com>
2019-03-16 16:21:00 +0330

    Fixes Billing class

--- q3/Billing.java
+++ q3/Billing.java
@@ -1,4 +1,17 @@
 package com.maktab.HW4.q3;
 
 public class Billing {
+    public static double computeBill(int price) {
+        return (price * 1.08);
+    }
+
+    public static double computeBill(int price, int quantity) {
+        return (price * quantity * 1.08);
+
+    }
+
+    public static double computeBill(int price, int quantity, int coupon) {
+        return (price * quantity * (1 - coupon) * 1.08);
+
+    }
 }
