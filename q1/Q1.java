c944f4a55f3895824a5aa6d9b6a95392cdc9c931
kazemian <mkzn94@gmail.com>
2019-03-16 15:12:17 +0330

    Creates new object from encryption class

--- q1/Q1.java
+++ q1/Q1.java
@@ -1,9 +1,10 @@
 package com.maktab.HW4.q1;
 
-import java.util.Scanner;
-
 public class Q1 {
     public static void main(String[] args) {
+
+        Encryption encryptor = new Encryption();
+        encryptor.encryptAll(encryptor.getMessage(),encryptor.getCode());
 
     }
 }
