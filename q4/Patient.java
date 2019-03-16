669edf76531bd70ff589baf9f423096e03a3ad2a
kazemian <mkzn94@gmail.com>
2019-03-16 17:42:32 +0330

    Fixes Patient class

--- q4/Patient.java
+++ q4/Patient.java
@@ -1,4 +1,43 @@
 package com.maktab.HW4.q4;
 
+
 public class Patient {
+    int ID, age;
+    BloodData bloodData;
+
+    public Patient() {
+        this.ID = 0;
+        this.age = 0;
+    }
+
+    public Patient(int ID, int age, String bloodType, String Rh) {
+        this.ID = ID;
+        this.age = age;
+        this.bloodData.setBloodType(bloodType);
+        this.bloodData.setRhFactor(Rh);
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public BloodData getBloodData() {
+        return bloodData;
+    }
+
+    public void setBloodData(BloodData bloodData) {
+        this.bloodData = bloodData;
+    }
 }
