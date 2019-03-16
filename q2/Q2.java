a7b19d49700a1508e2a3878b3191457872e3606d
kazemian <mkzn94@gmail.com>
2019-03-16 15:51:53 +0330

    Adds main mathod

--- q2/Q2.java
+++ q2/Q2.java
@@ -1,4 +1,21 @@
 package com.maktab.HW4.q2;
 
+import java.util.Scanner;
+
 public class Q2 {
+    public static void main(String[] args) {
+
+        String firstName, lastName;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your first name: ");
+        firstName = scanner.next();
+        System.out.println("Please enter your first name: ");
+        lastName = scanner.next();
+
+        FormLetterWriter.displaySalutation(lastName);
+        FormLetterWriter.displaySalutation(firstName, lastName);
+
+
+    }
+
 }
