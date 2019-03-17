07c1ae8e18206ba2afef85a5ee41e5a369fda860
kazemian <mkzn94@gmail.com>
2019-03-17 09:38:33 +0330

    Adds comments

--- q4/BloodData.java
+++ q4/BloodData.java
@@ -1,25 +1,31 @@
 package com.maktab.HW4.q4;
 
 public class BloodData {
+
+    //BloodData class' instances
     private String[] bloodTypes = {"O", "A", "B", "AB"};
     private String bloodType;
     private String RhFactor;
 
+    //first overloaded constructor
     public BloodData() {
         this.bloodType = "O";
         this.RhFactor = "+";
     }
 
+    //second overloaded constructor
     public BloodData(String bloodType, String RhFactor) {
         setBloodType(bloodType);
         setRhFactor(RhFactor);
     }
 
+    /*getters and setters*/
     public String getBloodType() {
         return bloodType;
     }
 
     public void setBloodType(String bloodType) {
+        //adding constrains so the user couldn't enter invalid inputs
         int k = 0;
         for (int i = 0; i < 4; i++) {
             if (bloodType.equals(bloodTypes[i])) {
@@ -37,9 +43,10 @@ public class BloodData {
     }
 
     public void setRhFactor(String RhFactor) {
+        //adding constrains so the user couldn't enter invalid inputs
         if (RhFactor.equals("+") || RhFactor.equals("-")) {
             this.RhFactor = RhFactor;
         } else
             System.out.println("invalid RhFactor!");
     }
-}
+}//end of BloodData class
