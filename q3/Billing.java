4bcae06ad517866856f1a34bb93450af2c2028d0
kazemian <mkzn94@gmail.com>
2019-03-17 09:20:52 +0330

    Adds comments

--- q3/Billing.java
+++ q3/Billing.java
@@ -1,17 +1,20 @@
 package com.maktab.HW4.q3;
 
 public class Billing {
+    //first overloaded constructor
     public static double computeBill(int price) {
         return (price * 1.08);
     }
 
+    //second overloaded constructor
     public static double computeBill(int price, int quantity) {
         return (price * quantity * 1.08);
 
     }
 
+    //third overloaded constructor
     public static double computeBill(int price, int quantity, int coupon) {
         return (price * quantity * (1 - 0.01*coupon) * 1.08);
 
     }
-}
+}//end of Billing class
