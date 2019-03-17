227e204eba3aed195b0f3d1e9413711aa9fefcf4
kazemian <mkzn94@gmail.com>
2019-03-17 08:13:00 +0330

    Adds comments

--- q1/Q1.java
+++ q1/Q1.java
@@ -2,9 +2,11 @@ package com.maktab.HW4.q1;
 
 public class Q1 {
     public static void main(String[] args) {
-
+        //making a new Encryption object
         Encryption encryptor = new Encryption();
+
+        //getting message and code from user to do the encryption
         encryptor.encryptAll(encryptor.getMessage(), encryptor.getCode());
 
-    }
-}
+    }//end of main method
+}//end of Q1 class
