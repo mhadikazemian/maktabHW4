650ce76739ae31fa71608d7bd8270146ee64592d
kazemian <mkzn94@gmail.com>
2019-03-17 09:21:07 +0330

    Adds comments

--- q3/Q3.java
+++ q3/Q3.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Q3 {
     public static void main(String[] args) {
         int price, quantity, coupon;
+
+        //getting inputs from user
         System.out.println("Enter the price of photo book: ");
         Scanner scanner = new Scanner(System.in);
         price = scanner.nextInt();
@@ -13,17 +15,20 @@ public class Q3 {
         System.out.println("Enter the discount rate: ");
         coupon = scanner.nextInt();
 
+        //testing first overloaded constructor
         System.out.println("First overload method: ");
         System.out.println(Billing.computeBill(price));
         System.out.println("*****");
 
+        //testing second overloaded constructor
         System.out.println("Second overload method: ");
         System.out.println(Billing.computeBill(price, quantity));
         System.out.println("*****");
 
+        //testing third overloaded constructor
         System.out.println("Third overload method: ");
         System.out.println(Billing.computeBill(price, quantity, coupon));
         System.out.println("*****");
 
-    }
-}
+    }//end of main method
+}//end of Q3 method
