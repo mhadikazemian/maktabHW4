9ce570833583d7b058784d0c3e7c43311637f73c
kazemian <mkzn94@gmail.com>
2019-03-17 08:30:07 +0330

    Adds comments

--- q2/FormLetterWriter.java
+++ q2/FormLetterWriter.java
@@ -2,15 +2,17 @@ package com.maktab.HW4.q2;
 
 public class FormLetterWriter {
 
+    //First overloaded method
     public static void displaySalutation(String lastName) {
         System.out.println("Dear Mr. or Ms " + lastName);
         System.out.println("Thank you for your recent order");
         System.out.println("*******************");
     }
 
+    //Second overload method
     public static void displaySalutation(String firstName, String lastName) {
         System.out.println("Dear " + firstName + " " + lastName);
         System.out.println("Thank you for your recent order");
 
     }
-}
+}//end of FormLetterWriter class
