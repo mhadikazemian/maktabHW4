e76eb412e324b4dd9d991996dca1c66b28a71bdf
kazemian <mkzn94@gmail.com>
2019-03-17 09:39:05 +0330

    Adds comments

--- q4/Q4.java
+++ q4/Q4.java
@@ -2,20 +2,28 @@ package com.maktab.HW4.q4;
 
 public class Q4 {
     public static void main(String[] args) {
+
+        //Testing first BloodData constructor
         System.out.println("First BloodData constructor: ");
         BloodData bloodData1 = new BloodData();
         System.out.println("Blood Type: " + bloodData1.getBloodType() + " Rh Factor: " + bloodData1.getRhFactor());
         System.out.println("*************");
+
+        //Testing second BloodData constructor
         System.out.println("Second BloodData constructor: ");
         BloodData bloodData2 = new BloodData("AB", "+");
         System.out.println(" Blood Type: " + bloodData2.getBloodType() + " Rh Factor: " + bloodData2.getRhFactor());
         System.out.println("*************");
+
+        //Testing first Patient constructor
         System.out.println("First Patient constructor: ");
         Patient patient1 = new Patient();
         System.out.println("Age: " + patient1.getAge() + " ID: " + patient1.getID());
         System.out.println("Blood Type: " + patient1.bloodData.getBloodType());
         System.out.println("Rh Factor: " + patient1.bloodData.getRhFactor());
         System.out.println("*************");
+
+        //Testing second Patient constructor
         System.out.println("Second Patient constructor: ");
         Patient patient2 = new Patient(1, 25, "A", "+");
         System.out.println("Age: " + patient2.getAge() + " ID: " + patient2.getID());
@@ -24,5 +32,5 @@ public class Q4 {
         System.out.println("*************");
 
 
-    }
-}
+    }//end of main method
+}//end of Q4 class
