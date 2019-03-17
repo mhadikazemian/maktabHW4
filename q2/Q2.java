29e8f8fdc4f8c776a4448b980f002565eb86b45e
kazemian <mkzn94@gmail.com>
2019-03-17 08:29:53 +0330

    Adds comments

--- q2/Q2.java
+++ q2/Q2.java
@@ -7,15 +7,18 @@ public class Q2 {
 
         String firstName, lastName;
         Scanner scanner = new Scanner(System.in);
+
+        //getting first name and last name from user
         System.out.println("Please enter your first name: ");
         firstName = scanner.next();
-        System.out.println("Please enter your first name: ");
+        System.out.println("Please enter your last name: ");
         lastName = scanner.next();
 
+        //testing two overloaded methods of FormLetterWriter class
         FormLetterWriter.displaySalutation(lastName);
         FormLetterWriter.displaySalutation(firstName, lastName);
 
 
-    }
+    }//end of main method
 
-}
+}//end of Q2 class
